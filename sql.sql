293ee52f917fe28b5f5179d098053733c86202e3
Kaitale Muhammad <137487556+kaitale-muhammad@users.noreply.github.com>
2024-10-17 17:47:00 +0300

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,6 @@
+create database demo;
+
+use demo;
+
+
+create  table services (id int not null primary key auto_increment, service_name varchar(20), image varchar(255) , descripton varchar(255), status varchar(100));
