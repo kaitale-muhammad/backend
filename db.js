147898ec8e1d990a64d117e6729b23a9e92863a4
MutangiziRonald <138402878+MutangiziRonald@users.noreply.github.com>
2024-10-24 20:53:03 +0300

    added Image functoionality

--- db.js
+++ db.js
@@ -1,73 +1,66 @@
-const express = require('express')
-const app = express()
-const multer = require('multer');
-const http = require('http')
-const server = http.createServer(app)
-const mysql = require('mysql2')
-const cors = require('cors')
-app.use(express.json())
-app.use(cors())
-
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const http = require("http");
+const server = http.createServer(app);
+const mysql = require("mysql2");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+const path = require("path");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    password: "17896",
-    database: "demo",
-    user : "root"
-})
+  host: "localhost",
+  password: "okronie89",
+  database: "demo",
+  user: "root",
+});
 
-db.connect((err)=>{
-    //console.log(err)
+db.connect((err) => {
+  //console.log(err)
 
-    console.log("Connected")
-    
-})
-
+  console.log("Connected");
+});
+app.use("/imgs", express.static(path.join(__dirname, "uploads")));
 //// Services
-app.get('/services',(req,res)=>{
-    
-    db.query("select * from services", (err, data)=>{
-        if(data){
-            console.log(data);
+app.get("/services", (req, res) => {
+  db.query("select * from services", (err, data) => {
+    if (data) {
+      return res.send(data);
+    }
 
-            return res.send(data)
-
-            
-        }
-        return res.status(500).send(err)
-    })
-})
+    return res.status(500).send(err);
+  });
+});
 
 // Create a new services
-app.post('/addservices', (req, res) => {
-    const { service_name, image, description , status} = req.body;
-    const sql = 'INSERT INTO services (service_name, image, descripton, status) VALUES (?, ?, ?, ?)';
-    db.query(sql, [service_name, image, description,status], (err, data) => {
-        if (data){
-            console.log(data);
+app.post("/addservices", (req, res) => {
+  const { service_name, image, description, status } = req.body;
+  const sql =
+    "INSERT INTO services (service_name, image, descripton, status) VALUES (?, ?, ?, ?)";
+  db.query(sql, [service_name, image, description, status], (err, data) => {
+    if (data) {
+      console.log(data);
 
-            return res.send(data)
-        } 
-        return res.status(500).send(err)
-       
-    });
+      return res.send(data);
+    }
+    return res.status(500).send(err);
+  });
 });
 
 ///  delete user
-app.delete('/services/:id', (req, res) => {
-    const sql = 'DELETE FROM services WHERE id = ?';
-    const id = Number(req.params.id);
-    db.query(sql, [id], (err, result) => {
-        if (err) throw err;
-        res.send(result);
-    });
+app.delete("/services/:id", (req, res) => {
+  const sql = "DELETE FROM services WHERE id = ?";
+  const id = Number(req.params.id);
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 // app.get('/add', (req, res) => {
 //     res.json({file:"Some data"})
 // });
-
-
 
 // Set up storage and file filter for multer
 // const storage = multer.diskStorage({
@@ -79,21 +72,21 @@ app.delete('/services/:id', (req, res) => {
 //     }
 //   });
 
-  const fileFilter = (req, file, cb) => {
-    const allowedTypes =["image/jpeg","image/jpg","image/png"];
-    if(!allowedTypes.includes(file.mimetype)){
-        const error = new Error("Incorrect File")
-        error.code = "INCORRECT FILE TYPE"
-        return cb(error,false);
-    }
-    cb(null,true)
-  
-    // if (extname && mimetype) {
-    //   cb(null, true);
-    // } else {
-    //   cb(new Error('Only images are allowed'));
-    // }
-  };
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Incorrect File");
+    error.code = "INCORRECT FILE TYPE";
+    return cb(error, false);
+  }
+  cb(null, true);
+
+  // if (extname && mimetype) {
+  //   cb(null, true);
+  // } else {
+  //   cb(new Error('Only images are allowed'));
+  // }
+};
 
 //   const upload = multer({
 //     storage: storage,
@@ -102,36 +95,73 @@ app.delete('/services/:id', (req, res) => {
 //   });
 
 const uploads = multer({
-    dest:'uploads/',
-    fileFilter,
-    limits:{
-        fileSize: 1000000
+  dest: "uploads/",
+  fileFilter,
+  limits: {
+    fileSize: 1000000,
+  },
+});
+
+// Image upload route
+app.post("/uploads", uploads.single("file"), (req, res) => {
+  res.json({ file: req.file });
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "LIMIT_FILE_SIZE") {
+    res.status(422).json({ error: "Allowed file size is 1000/KB" });
+    return;
+  }
+});
+
+app.get("/services/:id", (req, res) => {
+  const { id } = req.params;
+  sql = "select * from services where id = ?";
+  db.query(sql, [id], (err, data) => {
+    if (data) {
+      return res.send(data[0]);
     }
-})
-
-  // Image upload route
-app.post('/uploads',uploads.single("file"), (req, res)=>{
-    res.json({file:req.file})
-})
- 
-
-app.use((err,req,res,next)=>{
-    if(err.code === "LIMIT_FILE_SIZE"){
-        res.status(422).json({error: "Allowed file size is 1000/KB"})
-        return;
+    res.send(err);
+  });
+});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+const fileUpload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
+app.put("/services/:id", fileUpload.single("file"), (req, res) => {
+  const id = req.params.id;
+  const { description, image, service_name, status } = req.body;
+  const file = req?.file?.filename ?? image;
+  console.log(file);
+  var sql = `UPDATE services
+SET service_name = ?,
+    image = ?,
+    descripton = ?,
+    status = ?
+WHERE id = ?`;
+  db.query(sql, [service_name, file, description, status, id], (err, data) => {
+    if (data) {
+      return res.sendStatus(201);
     }
-})
+    console.log(err);
 
+    res.sendStatus(500);
+  });
+});
+console.log(path.join(__dirname, "uploads/"));
 
-app.get('/services/:id',(req,res)=>{
-    const {id} = req.params
-    sql = "select * from services where id = ?"
-    db.query(sql, [id],(data,err)=>{
-        console.log(data)
-    })
-})
-
-server.listen(5000 , ()=>{
-    console.log("listening");
-    
-})
+app.use("/uploads", express.static(path.join(__dirname, "uploads/")));
+server.listen(5000, () => {
+  console.log("listening");
+});
