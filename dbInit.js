5b921ad87b6f934cd9a66f133106e8c289f99bca
Kaitale Muhammad <137487556+kaitale-muhammad@users.noreply.github.com>
2024-12-07 15:20:23 +0300

    Update dbInit.js

--- dbInit.js
+++ dbInit.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql2");
 // const mysql = require("mysql2/promise");
+// const db = mysql.createConnection({
+//   host: "sql12.freesqldatabase.com",
+//   password: "feGLj91JhT",
+//   database: "sql12747182",
+//   user: "sql12747182",
+//   port: 3306,
+// });
 const db = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  password: "feGLj91JhT",
-  database: "sql12747182",
-  user: "sql12747182",
-  port: 3306,
+  host: "autorack.proxy.rlwy.net",
+  password: "UhWMkmGJmpdunxKEfKxQWqAhbnmHlcSW",
+  database: "railway",
+  user: "root",
+  port: 39025,
 });
-
+//mysql://root:UhWMkmGJmpdunxKEfKxQWqAhbnmHlcSW@autorack.proxy.rlwy.net:39025/railway
 db.connect((err) => {
   console.log("Connected");
   console.log(err);
